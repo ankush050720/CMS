d3925422f9a8f2a73d426882ae6e005c42ef41fe
Ankush Jha <ankushjha@iitbhilai.ac.in>
2024-10-23 00:13:07 +0530

    update

--- chat-Integration/backend/server.js
+++ chat-Integration/backend/server.js
@@ -28,19 +28,13 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
