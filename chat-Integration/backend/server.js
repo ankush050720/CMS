006e4e0dfe4f2e1e7d5696bf96bc30731814dc84
Ankush Jha <ankushjha@iitbhilai.ac.in>
2024-10-23 00:08:40 +0530

    update

--- chat-Integration/backend/server.js
+++ chat-Integration/backend/server.js
@@ -40,7 +40,7 @@ const __dirname1 = path.resolve();
 //   });
 // }
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
