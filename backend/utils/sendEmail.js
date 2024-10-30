efebeb55d04c3d361f2913cfddc23cfe756076e2
Ankush Jha <ankushjha@iitbhilai.ac.in>
2024-10-30 13:28:30 +0530

    update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,22 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  // Validate options
   if (!options || !options.email || !options.subject || !options.message) {
     throw new Error('Missing required email options');
   }
 
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', // or another email service
+    host: 'smtpout.secureserver.net',
+    port: 465,
+    secure: true, // Use true since you're connecting with SSL/TLS on port 465
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER, // Update if hardcoding
+      pass: process.env.EMAIL_PASS, // Ensure this matches your GoDaddy email password
     },
   });
 
   const mailOptions = {
-    from: 'SRU <ankjha1507@gmail.com>',
+    from: 'SRU <noreply@sruclub.in>',
     to: options.email,
     subject: options.subject,
     text: options.message,
@@ -23,9 +24,10 @@ const sendEmail = async (options) => {
 
   try {
     await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully!');
   } catch (err) {
     console.error('Error sending email:', err);
-    throw err; // Re-throw to handle it in the calling code
+    throw err;
   }
 };
 
