1be7bee0eb00ed0a4169af56e5ce6f0d068cdb89
Ankush Jha <ankushjha@iitbhilai.ac.in>
2024-11-22 01:34:04 +0530

    update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -50,8 +50,7 @@ exports.login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === 'production',
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'None',
       path: '/',
     });
