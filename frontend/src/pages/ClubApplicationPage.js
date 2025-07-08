d3cfde443bfc0bf6b6bdd3186caaaea826bb8649
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2025-07-08 18:47:43 +0530

    Update ClubApplicationPage.js

--- frontend/src/pages/ClubApplicationPage.js
+++ frontend/src/pages/ClubApplicationPage.js
@@ -14,7 +14,7 @@ import { submitApplication } from "../services/applicationService";
 import LoadingButton from "../components/LoadingButton";
 import LoadingForm from "../components/LoadingForm";
 import FileUpload from '../utils/FileUpload';
-import { uploadFileToCloudinary } from '../utils/cloudinaryUpload';
+import { uploadImageToCloudinary } from '../utils/cloudinaryUpload';
 
 const styles = (
   <style>
