6db2562dbe72066e9c607df1a057f7db86612932
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2025-07-08 18:35:29 +0530

    Update cloudinaryUpload.js

--- frontend/src/utils/cloudinaryUpload.js
+++ frontend/src/utils/cloudinaryUpload.js
@@ -1,23 +1,25 @@
+// cloudinaryUpload.js
 import axios from 'axios';
 
 const CLOUDINARY_URL = process.env.REACT_APP_CLOUDINARY_URL;
 const CLOUDINARY_PRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
 
-export const uploadImageToCloudinary = async (file) => {
+export const uploadFileToCloudinary = async (file) => {
   const formData = new FormData();
   formData.append('file', file);
   formData.append('upload_preset', CLOUDINARY_PRESET);
   formData.append('cloud_name', 'dsailrfh1');
+
   try {
     const response = await axios.post(CLOUDINARY_URL, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-      withCredentials: false, // Adjust this based on your needs
     });
-    return response.data.url; // Return the secure URL of the uploaded image
+
+    return response.data.secure_url; // Use secure_url for HTTPS-safe link
   } catch (error) {
-    console.error('Error uploading image:', error);
-    throw new Error('Image upload failed');
+    console.error('Error uploading file:', error);
+    throw new Error('File upload failed');
   }
 };
