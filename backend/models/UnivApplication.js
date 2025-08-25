12dee7d7c361e553fc1c6068e5012f842767a170
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2025-08-25 09:58:52 +0530

    Create UnivApplication.js

---
+++ backend/models/UnivApplication.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the application model
+const univApplicationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  hallTicket: {
+    type: String,
+    required: true,
+  },
+  clubName: {
+    type: [String],
+    required: true,
+  },
+  reason: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    type: String,
+    default: '',
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  // New fields
+  program: String,
+  year: String,
+  specialization: String,
+  recommender1: String,
+  recommender2: String,
+  linkedin: String,
+  facebook: String,
+  instagram: String,
+  other_media: String,
+  github: String,
+  youtube: String,
+  cv: String,
+  comment: {
+    type: String,
+    default: '',
+  },
+});
+
+const UnivApplication = mongoose.model('UnivApplication', univApplicationSchema);
+
+module.exports = UnivApplication;
