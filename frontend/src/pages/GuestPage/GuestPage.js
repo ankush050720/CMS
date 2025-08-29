935856cc64bab6a119b5da4950317e83f57786de
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2025-08-29 09:54:15 +0530

    Update GuestPage.js

--- frontend/src/pages/GuestPage/GuestPage.js
+++ frontend/src/pages/GuestPage/GuestPage.js
@@ -43,7 +43,7 @@ const GuestPage = () => {
         to apply!
       </div>
 
-      <div className="announcement">
+      <div className="announcement2">
         University Club Registration is active now. Click{" "}
         <Link component={RouterLink} to="/univ-club-registration" className="highlight-link">
           Here
